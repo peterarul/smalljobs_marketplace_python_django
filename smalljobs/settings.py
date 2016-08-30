af37a18b1484bab1f5818b706bb859494da27f96
Arul Peter <arulpeter@Aruls-MacBook-Air.local>
2016-08-30 01:12:52 -0400

    minor setting changes

--- smalljobs/settings.py
+++ smalljobs/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'smalljobs.wsgi.application'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
